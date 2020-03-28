1c6b2e69600c7efd2552b78da01ca0700f2f9980
Glitch (hello-express) <none>
2020-03-28 13:06:01 +0000

    🌉🚇 Checkpoint ./config.json:610302/8 ./server.js:610302/27

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
+  console.log(msg);
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
 
